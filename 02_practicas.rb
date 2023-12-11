28fe5c17b1d7d6cdb640877e4dfb1bacbe7db0fe
Totenkopf1995 <uroxacin1@hotmail.com>
2023-12-11 01:02:53 -0500

    practicas POO

---
+++ 02_practicas.rb
@@ -0,0 +1,23 @@
+class Motion
+  def initialize(start, stop)
+    @start = start
+    @stop = stop
+  end
+  attr_accessor :start, :stop
+  
+  def forward_movement
+    
+  end
+  
+  def backward_movement
+    
+  end
+  
+  def movement_to_the_right
+    
+  end
+  
+  def movement_to_the_lef
+    
+  end
+end
